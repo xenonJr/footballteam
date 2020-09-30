d76a07393bf7c30b736bc0c773d5ede88a726058
xenonJr <61556431+xenonJr@users.noreply.github.com>
2020-09-30 13:37:33 +0600

    first init

---
+++ app/src/main/java/com/nadxlib/footballteam/MainActivity.java
@@ -0,0 +1,25 @@
+package com.nadxlib.footballteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button con ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        con = findViewById(R.id.cont_bt);
+        con.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),DetailActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/nadxlib/footballteam/WV.java
@@ -0,0 +1,66 @@
+package com.nadxlib.footballteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class WV extends AppCompatActivity {
+
+    WebView webView;
+    ImageView back;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        back =   findViewById(R.id.backButton_web);
+        textView = findViewById(R.id.desc);
+         webView = findViewById(R.id.web);
+
+        String name = getIntent().getStringExtra("name");
+        String link = getIntent().getStringExtra("url");
+
+        Toast.makeText(getApplicationContext(),"Loading ",Toast.LENGTH_LONG).show();
+
+        textView.setText(name);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        final Activity activity = this;
+
+        webView.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(android.webkit.WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(android.webkit.WebView view, WebResourceRequest req, WebResourceError rerr) {
+                // Redirect to deprecated method, so you can use it in all SDK versions
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+
+        });
+
+        webView.loadUrl(link);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),DetailActivity.class));
+            }
+        });
+
+    }
+}
