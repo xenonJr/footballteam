cadce0c249d36ab38e23a681e625d586bb90831f
xenonjr <zkxenon98@gmail.com>
2020-12-24 17:11:54 +0600

    babycare

---
+++ app/src/main/java/com/nadxlib/mnu/home.java
@@ -0,0 +1,33 @@
+package com.nadxlib.mnu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Button user = findViewById(R.id.cont_bt);
+        Button admin = findViewById(R.id.button6);
+
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),AdminLogin.class));
+            }
+        });
+    }
+}
